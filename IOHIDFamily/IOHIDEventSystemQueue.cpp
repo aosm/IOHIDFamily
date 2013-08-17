7dc2f2044e6b816212c7cef2b5e8abf11d4b1900
Rasmus Sten <rasmus@dll.nu>
2013-08-17 15:33:55 +0300

    version 421.24.2

--- IOHIDFamily/IOHIDEventSystemQueue.cpp
+++ IOHIDFamily/IOHIDEventSystemQueue.cpp
@@ -1,16 +1,15 @@
 /*
- *
  * @APPLE_LICENSE_HEADER_START@
- * 
+ *
  * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
+ *
  * This file contains Original Code and/or Modifications of Original Code
  * as defined in and that are subject to the Apple Public Source License
  * Version 2.0 (the 'License'). You may not use this file except in
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,40 +17,37 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  */
 
-#ifndef _IOKIT_HID_IOHIDEVENTSERVICEQUEUE_H
-#define _IOKIT_HID_IOHIDEVENTSERVICEQUEUE_H
+#undef enqueue
+#include "IOHIDEventSystemQueue.h"
 
-#include <IOKit/IOSharedDataQueue.h>
+#define super IOSharedDataQueue
+OSDefineMetaClassAndStructors( IOHIDEventSystemQueue, super )
 
-class IOHIDEvent;
 //---------------------------------------------------------------------------
-// IOHIDEventSeviceQueue class.
-//
-// IOHIDEventServiceQueue is a subclass of IOSharedDataQueue.
-
-class IOHIDEventServiceQueue: public IOSharedDataQueue
+Boolean IOHIDEventSystemQueue::enqueue(void *data, UInt32 dataSize)
 {
-    OSDeclareDefaultStructors( IOHIDEventServiceQueue )
+    Boolean             result  = false;
     
-protected:
-    IOMemoryDescriptor *    _descriptor;
-    Boolean                 _state;
-
-public:
-    static IOHIDEventServiceQueue *withCapacity(UInt32 size);
-    virtual void free();
+    _didSendNotificationForLastEnqueue = false;
     
-    inline Boolean getState() { return _state; }
-    inline void setState(Boolean state) { _state = state; }
+    result = super::enqueue(data, dataSize);
+    if (!_didSendNotificationForLastEnqueue) {
+        //kprintf("IOHIDEventSystemQueue sending notification for IOSharedDataQueue.\n");
+        sendDataAvailableNotification();
+    }
+    
+    return result;
+}
 
-    virtual Boolean enqueueEvent(IOHIDEvent * event);
+//---------------------------------------------------------------------------
+void IOHIDEventSystemQueue::sendDataAvailableNotification()
+{
+    super::sendDataAvailableNotification();
+    _didSendNotificationForLastEnqueue = true;
+}
 
-    virtual IOMemoryDescriptor *getMemoryDescriptor();
-    virtual void setNotificationPort(mach_port_t port);
-};
-
-#endif /* !_IOKIT_HID_IOHIDEVENTSERVICEQUEUE_H */
+//---------------------------------------------------------------------------
--- IOHIDFamily/IOHIDEventSystemQueue.h
+++ IOHIDFamily/IOHIDEventSystemQueue.h
@@ -1,16 +1,16 @@
 /*
  *
  * @APPLE_LICENSE_HEADER_START@
- * 
+ *
  * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
+ *
  * This file contains Original Code and/or Modifications of Original Code
  * as defined in and that are subject to the Apple Public Source License
  * Version 2.0 (the 'License'). You may not use this file except in
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,40 +18,28 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  */
 
-#ifndef _IOKIT_HID_IOHIDEVENTSERVICEQUEUE_H
-#define _IOKIT_HID_IOHIDEVENTSERVICEQUEUE_H
+#ifndef _IOKIT_HID_IOHIDEVENTSYSTEMQUEUE_H
+#define _IOKIT_HID_IOHIDEVENTSYSTEMQUEUE_H
 
 #include <IOKit/IOSharedDataQueue.h>
 
-class IOHIDEvent;
 //---------------------------------------------------------------------------
-// IOHIDEventSeviceQueue class.
-//
-// IOHIDEventServiceQueue is a subclass of IOSharedDataQueue.
-
-class IOHIDEventServiceQueue: public IOSharedDataQueue
+class IOHIDEventSystemQueue: public IOSharedDataQueue
 {
-    OSDeclareDefaultStructors( IOHIDEventServiceQueue )
+    OSDeclareDefaultStructors( IOHIDEventSystemQueue )
+    
+    bool _didSendNotificationForLastEnqueue;
     
 protected:
-    IOMemoryDescriptor *    _descriptor;
-    Boolean                 _state;
-
-public:
-    static IOHIDEventServiceQueue *withCapacity(UInt32 size);
-    virtual void free();
+    virtual void sendDataAvailableNotification();
     
-    inline Boolean getState() { return _state; }
-    inline void setState(Boolean state) { _state = state; }
-
-    virtual Boolean enqueueEvent(IOHIDEvent * event);
-
-    virtual IOMemoryDescriptor *getMemoryDescriptor();
-    virtual void setNotificationPort(mach_port_t port);
+public:
+    virtual Boolean enqueue(void *data, UInt32 dataSize);
 };
 
-#endif /* !_IOKIT_HID_IOHIDEVENTSERVICEQUEUE_H */
+//---------------------------------------------------------------------------
+#endif /* !_IOKIT_HID_IOHIDEVENTSYSTEMQUEUE_H */
